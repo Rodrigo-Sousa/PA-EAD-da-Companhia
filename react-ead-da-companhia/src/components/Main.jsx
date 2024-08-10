8cb0968edd0b6899a10f48d1149df054c6f464ff
Rodrigo Sousa <rodrigo.sdeveloper@gmail.com>
2024-08-10 14:39:35 -0300

    Corrigido os espaçamentos no container e os cards

--- react-ead-da-companhia/src/components/Main.jsx
+++ react-ead-da-companhia/src/components/Main.jsx
@@ -1,3 +1,3 @@
 export default function Main({ children }) {
-  return <main className='mx-auto p-4 flex flex-wrap'>{children}</main>;
+  return <main className='mx-auto py-4 px-40 flex flex-wrap'>{children}</main>;
 }
