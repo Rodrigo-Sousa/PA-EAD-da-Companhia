3532a79d7298ba58830f1d032f18d4a0be5f6edf
Rodrigo Sousa <rodrigo.sdeveloper@gmail.com>
2024-08-24 15:49:39 -0300

    Adicionado o conteúdo do backend, para ser generado com o id do UUID

---
+++ backend/generator.js
@@ -0,0 +1,42 @@
+const uuid = require('uuid').v4;
+const fs = require('fs').promises;
+
+const allVideosClicados = [
+  {
+    tituloVideo: "pis-cofins",
+    qtdAssistido: 0
+  },
+  {
+    tituloVideo: "reforma-tributaria",
+    qtdAssistido: 0
+  },
+  {
+    tituloVideo: "imposto-sobre-servico-digitais",
+    qtdAssistido: 0
+  },
+  {
+    tituloVideo: "tributacao-investimentos",
+    qtdAssistido: 0
+  },
+  {
+    tituloVideo: "combate-evasao-fiscal",
+    qtdAssistido: 0
+  },
+  {
+    tituloVideo: "imposto-csll",
+    qtdAssistido: 0
+  }
+]
+  .map(item => {
+    return { id: uuid(), ...item };
+  })
+  .sort((a, b) => a.tituloVideo.localeCompare(b.tituloVideo));
+
+async function start() {
+  fs.writeFile(
+    './qtd-clicks-os-videos-receberam.json',
+    JSON.stringify({ videosClicados: allVideosClicados }, null, 2)
+  );
+}
+
+start();
